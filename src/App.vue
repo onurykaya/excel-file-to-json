de1b815f1b1e554316dff13053704ba315a9b31d
Onur Yerlikaya <onur.yerlikaya1@gmail.com>
2021-01-24 23:05:52 +0300

    added pharmcy app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <c-theme-provider>
+    <c-reset />
+    <!--Your application goes here -->
+    <Container>
+      <router-view />
+    </Container>
+  </c-theme-provider>
+</template>
+
+<script>
+import { CThemeProvider, CReset } from "@chakra-ui/vue";
+import Container from "@/components/Container";
+export default {
+  name: "App",
+  components: {
+    Container,
+    CThemeProvider,
+    CReset
+  }
+};
+</script>
